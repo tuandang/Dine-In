06b33854cb81810332d4d08824696769c8f9e87e
tuandang <a4dang@ucsd.edu>
2014-02-08 05:59:02 -0800

    Create order.php

---
+++ order.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title> Tuan's Desk </title>
+	<link rel="stylesheet" href="css2/grid.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css2/style2.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css2/normalize.css" type="text/css" media="screen">
+    <!-- google web fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
+
+
+
+</head>
+
+<body>
+
+
+
+
+
+</body>
+</html>
