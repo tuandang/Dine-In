bcc81c2b6d572440878198f4a6e1663bf34633a9
tuandang <a4dang@ucsd.edu>
2014-02-08 05:56:49 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+
+
+<html>
+<head>
+	<title> Dine-In
+	</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+	<link rel="shortcut icon" href="favicon.ico">
+
+
+</head>
+<body >
+
+	<br><br><br><br><br><br><br>
+
+	<div id="head"><h1 id="head"> <i> Dine-In </i></h1><br></div>
+	<div align="center" id="content">
+
+	<form  action="index2.php" method="get">
+
+
+		 <th><label for="name">Please type in your Table ID: </label></th> 
+		<td><input type="text" name="tableId" /></td>
+
+
+
+		<input type="submit" value="Sign In">
+
+	</form>
+
+
+
+
+	</div>
+
+
+
+</body>
+</html> 
